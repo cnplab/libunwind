b2098120073de8d21fdf111b48ab4dafb3010c4b
Florian Schmidt <florian.schmidt@neclab.eu>
2017-03-07 17:47:17 +0100

    xen: Use segbase and offset from UXEN_info
    While the patch that extracted the information from the ELF file and put
    it into UXEN_info has been there for a long time, we didn't actually
    make use of it. D'oh!

--- src/xen/_UXEN_get_proc_name.c
+++ src/xen/_UXEN_get_proc_name.c
@@ -9,6 +9,13 @@ _UXEN_get_proc_name (unw_addr_space_t as, unw_word_t addr, char *bufp, size_t bu
   Debug (16, "as=%p, addr=0x%lx, bufp=%p, buflen=%lu, offp=%p, arg=%p\n",
          as, addr, bufp, buflen, offp, arg);
 
-  //TODO get segbase and offset from the ELF file
-  return _Uelf64_get_proc_name_in_image (as, &ui->edi.ei, 0x1000, 0, addr, bufp, buflen, offp);
+#if ELF_CLASS == ELFCLASS64
+  return _Uelf64_get_proc_name_in_image (as, &ui->edi.ei, ui->baseaddr, ui->offset,
+                                         addr, bufp, buflen, offp);
+#elif ELF_CLASS == ELFCLASS32
+  return _Uelf32_get_proc_name_in_image (as, &ui->edi.ei, ui->baseaddr, ui->offset,
+                                         addr, bufp, buflen, offp);
+#else
+  return -UNW_ENOINFO;
+#endif
 }
