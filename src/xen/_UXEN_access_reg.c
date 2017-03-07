7895fbc9656222e6b8bde0561f7ec605dc04a3bc
Florian Schmidt <florian.schmidt@neclab.eu>
2017-03-07 11:02:08 +0100

    xen: _UXEN_access_reg: generalize for all registers

--- src/xen/_UXEN_access_reg.c
+++ src/xen/_UXEN_access_reg.c
@@ -1,6 +1,103 @@
 #include <stdio.h>
 #include "_UXEN_internal.h"
 
+#define RETURN_REGISTER(regname)                                        \
+do {                                                                    \
+  Debug(6, "returning %s=0x%lx\n", #regname, ctx->user_regs.regname);   \
+  *valp = ctx->user_regs.regname;                                       \
+  return 0;                                                             \
+} while(0)
+
+#if defined(__i386__)
+int
+access_reg_x86 (vcpu_guest_context_t * ctx, unw_regnum_t regnum, unw_word_t * valp)
+{
+  switch (regnum)
+    {
+    case UNW_X86_EAX:
+      RETURN_REGISTER (eax);
+    case UNW_X86_EDX:
+      RETURN_REGISTER (edx);
+    case UNW_X86_ECX:
+      RETURN_REGISTER (ecx);
+    case UNW_X86_EBX:
+      RETURN_REGISTER (ebx);
+    case UNW_X86_ESI:
+      RETURN_REGISTER (esi);
+    case UNW_X86_EDI:
+      RETURN_REGISTER (edi);
+    case UNW_X86_EBP:
+      RETURN_REGISTER (ebp);
+    case UNW_X86_ESP:
+      RETURN_REGISTER (esp);
+    case UNW_X86_EIP:
+      RETURN_REGISTER (eip);
+    case UNW_X86_EFLAGS:
+      RETURN_REGISTER (eflags);
+    case UNW_X86_GS:
+      RETURN_REGISTER (gs);
+    case UNW_X86_FS:
+      RETURN_REGISTER (fs);
+    case UNW_X86_ES:
+      RETURN_REGISTER (es);
+    case UNW_X86_DS:
+      RETURN_REGISTER (ds);
+    case UNW_X86_SS:
+      RETURN_REGISTER (ss);
+    case UNW_X86_CS:
+      RETURN_REGISTER (cs);
+    default:
+      return -UNW_EBADREG;
+    }
+}
+#endif /* i386 */
+
+#if defined(__x86_64__)
+unw_word_t
+access_reg_x86_64 (vcpu_guest_context_t * ctx, unw_regnum_t regnum, unw_word_t * valp)
+{
+  switch (regnum)
+    {
+    case UNW_X86_64_RAX:
+      RETURN_REGISTER (rax);
+    case UNW_X86_64_RDX:
+      RETURN_REGISTER (rdx);
+    case UNW_X86_64_RCX:
+      RETURN_REGISTER (rcx);
+    case UNW_X86_64_RBX:
+      RETURN_REGISTER (rbx);
+    case UNW_X86_64_RSI:
+      RETURN_REGISTER (rsi);
+    case UNW_X86_64_RDI:
+      RETURN_REGISTER (rdi);
+    case UNW_X86_64_RBP:
+      RETURN_REGISTER (rbp);
+    case UNW_X86_64_RSP:
+      RETURN_REGISTER (rsp);
+    case UNW_X86_64_R8:
+      RETURN_REGISTER (r8);
+    case UNW_X86_64_R9:
+      RETURN_REGISTER (r9);
+    case UNW_X86_64_R10:
+      RETURN_REGISTER (r10);
+    case UNW_X86_64_R11:
+      RETURN_REGISTER (r11);
+    case UNW_X86_64_R12:
+      RETURN_REGISTER (r12);
+    case UNW_X86_64_R13:
+      RETURN_REGISTER (r13);
+    case UNW_X86_64_R14:
+      RETURN_REGISTER (r14);
+    case UNW_X86_64_R15:
+      RETURN_REGISTER (r15);
+    case UNW_X86_64_RIP:
+      RETURN_REGISTER (rip);
+    default:
+      return -UNW_EBADREG;
+    }
+}
+#endif /* x86_64 */
+
 int
 _UXEN_access_reg (unw_addr_space_t as, unw_regnum_t regnum, unw_word_t * valp, int write, void *arg)
 {
@@ -10,21 +107,11 @@ _UXEN_access_reg (unw_addr_space_t as, unw_regnum_t regnum, unw_word_t * valp, i
   Debug (16, "as=%p, regnum=%u, valp=%p, write=%u, arg=%p\n", as, regnum, valp, write, arg);
   if (UWXEN_get_vcpu_context (ui, &ctx))
     return -UNW_EINVAL;
-  switch (regnum)
-    {
-    case 6:
-      Debug (6, "returning RBP=0x%lx\n", ctx.user_regs.rbp);
-      *valp = ctx.user_regs.rbp;
-      return 0;
-    case 7:
-      Debug (6, "returning RSP=0x%lx\n", ctx.user_regs.rsp);
-      *valp = ctx.user_regs.rsp;
-      return 0;
-    case 16:
-      Debug (6, "returning RIP=0x%lx\n", ctx.user_regs.rip);
-      *valp = ctx.user_regs.rip;
-      return 0;
-    default:
-      return -UNW_EBADREG;
-    }
+#if defined(__x86_64__)
+  return access_reg_x86_64 (&ctx, regnum, valp);
+#elif defined(__i386__)
+  return access_reg_x86 (&ctx, regnum, valp);
+#else
+#warning "_UXEN_access_reg not implemented for this architecture!"
+#endif
 }
